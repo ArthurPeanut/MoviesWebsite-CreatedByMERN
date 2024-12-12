7c87bea417027fed74c3ba1e2ae8c11c27b2443c
Haoqing Liu <u7776608@anu.edu.au>
2024-12-12 16:28:55 +1100

    Created Movies Controller

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import MoviesController from './movies.controller.js'
 
 // Get access to express router
 const router = express.Router()
 
 // Every route in movies will start with /api/v1/movies, due to server.js
-router.route('/').get((req, res) => res.send('hello world'))
+router.route('/').get(MoviesController.apiGetMovies)
 
 export default router
